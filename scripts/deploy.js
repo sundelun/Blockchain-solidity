850794795abefaf46380bd1c66b550ee513abd9c
sdl <delun.alan.sun@gmail.com>
2025-04-15 22:05:25 -0400

    small fix on deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,11 +1,12 @@
 // scripts/deploy.js
-
+require('dotenv').config({path: "./scripts/.env"});
 async function main() {
     const [deployer] = await ethers.getSigners();
     console.log("Deploying DAI deposit contract with account:", deployer.address);
 
     // 2) point at the mock you just deployed (or real DAI on mainnet)
-    const tokenAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+    //const tokenAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+    const tokenAddress = process.env.MOCKDAI.toString();
 
     // deploy the DAI deposit contract
     const DAI = await ethers.getContractFactory("DAI", deployer);
